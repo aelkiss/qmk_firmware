6376846f4f5d93c0f5122c0d923d05aa3ec785d6
Aaron Elkiss <aelkiss@umich.edu>
2020-11-13 09:05:06 -0500

    BM60RGB: compose key unicode french char support

--- keyboards/bm60rgb/keymaps/aelkiss/keymap.c
+++ keyboards/bm60rgb/keymaps/aelkiss/keymap.c
@@ -6,14 +6,14 @@
  * edit it directly.
  */
 
-#define TAPPING_TOGGLE 2
+#include "process_leader.h"
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[0] = LAYOUT(KC_GESC, KC_1, KC_2, KC_3, KC_4, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_EQL, KC_BSPC,
                KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_BSLS,
                MO(1), KC_A, KC_S, KC_D, KC_F, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT,
                KC_LSFT, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_N, KC_M, KC_COMM, KC_DOT, RSFT_T(KC_SLSH), KC_SLSH, TG(2),
-               KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, OSL(4), MO(1), KC_RALT, MO(3), KC_RCTL),
+               KC_LCTL, KC_LGUI, KC_LALT, KC_SPC, KC_LEAD, MO(1), KC_RALT, MO(3), KC_RCTL),
 
 	[1] = LAYOUT(KC_GRV, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12,
                RESET, KC_TRNS, KC_HOME, KC_UP, KC_END, KC_PGUP, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_MUTE, KC_VOLD, KC_VOLU, KC_NO,
@@ -31,3 +31,56 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	[4] = LAYOUT(KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO)
 };
 
+LEADER_EXTERNS();
+
+void matrix_scan_user(void) {
+  LEADER_DICTIONARY() {
+    leading = false;
+    leader_end();
+
+    SEQ_TWO_KEYS(KC_GESC,KC_A) {
+      send_unicode_string("à");
+    }
+    SEQ_TWO_KEYS(KC_GESC,KC_E) {
+      send_unicode_string("è");
+    }
+
+    SEQ_TWO_KEYS(KC_QUOTE,KC_A) {
+      send_unicode_string("á");
+    }
+    SEQ_TWO_KEYS(KC_QUOTE,KC_E) {
+      send_unicode_string("é");
+    }
+
+    SEQ_TWO_KEYS(KC_6,KC_A) {
+      send_unicode_string("â");
+    }
+    SEQ_TWO_KEYS(KC_6,KC_E) {
+      send_unicode_string("ê");
+    }
+    SEQ_TWO_KEYS(KC_6,KC_I) {
+      send_unicode_string("î");
+    }
+    SEQ_TWO_KEYS(KC_6,KC_O) {
+      send_unicode_string("ô");
+    }
+    SEQ_TWO_KEYS(KC_6,KC_U) {
+      send_unicode_string("û");
+    }
+
+    SEQ_TWO_KEYS(KC_A,KC_E) {
+      send_unicode_string("æ");
+    }
+
+    SEQ_TWO_KEYS(KC_O,KC_E) {
+      send_unicode_string("œ");
+    }
+
+    SEQ_TWO_KEYS(KC_COMMA,KC_COMMA) {
+      send_unicode_string("«");
+    }
+    SEQ_TWO_KEYS(KC_DOT,KC_DOT) {
+      send_unicode_string("»");
+    }
+  }
+}
